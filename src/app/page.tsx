290b3ae119a66a2d3f66d6f1589f2452ad3b7369
Takuya Hirata <takuyahirata4@gmail.com>
2026-05-27 09:39:51 -0400

    Style: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+
+import ImgThree from './../../public/image5.png'
+import AppleStore from './../../public/apple-store.png'
+
+
+
 export default function Home() {
   return (
-    <main className="font-sans max-w-7xl mx-auto w-11/12 py-6 md:py-12">
-      hey
+    <main className="font-sans max-w-7xl mx-auto w-11/12 py-2 md:py-8">
+      <div className="grid gap-4 md:grid-cols-2 items-center">
+        <div>
+          <h1 className="font-semibold md:text-2xl">Track what matters with ActionStack</h1>
+          <a href="https://apps.apple.com/us/app/actionstack/id6765557900">
+            <Image src={AppleStore} alt="Apple store"
+              className="-ml-5 w-1/2"
+            />
+          </a>
+        </div>
+        <div className="flex justify-center">
+          <Image src={ImgThree} alt="ActionStack screenshot"
+            className="w-1/2 md:w-1/2"
+          />
+        </div>
+      </div>
+      <p className="mt-8">
+        ActionStack helps you track meaningful actions with a clean, focused experience. Log time-based sessions or repetition-based activities like workouts, study sessions, habits, and daily goals.
+      </p>
     </main>
   );
 }
